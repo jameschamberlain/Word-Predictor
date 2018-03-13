a27ff850614f7042fceb70a281cfaef07c915d97
jxc1089 <JXC1089@student.bham.ac.uk>
2018-03-13 17:49:51 +0000

    Project setup

---
+++ src/DictionaryTreeTests.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * @author Kelsey McKenna
+ */
+public class DictionaryTreeTests {
+
+    @Test
+    public void heightOfRootShouldBeZero() {
+        DictionaryTree unit = new DictionaryTree();
+        Assertions.assertEquals(0, unit.height());
+    }
+
+    @Test
+    public void heightOfWordShouldBeWordLength() {
+        DictionaryTree unit = new DictionaryTree();
+        unit.insert("word", 0);
+        Assertions.assertEquals("word".length(), unit.height());
+    }
+
+}
